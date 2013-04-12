dceaa7ed7232ac6171970929ae7ca817d154db07
abveritas <veritasfarm@gmail.com>
2013-04-12 15:21:21 +0000

    kmod update, sqlite rename, remove 3 & update, prep for linux 3.8 stable

--- linux/create-aufs.sh
+++ linux/create-aufs.sh
@@ -5,7 +5,7 @@ rm -Rf aufs3-standalone
 git clone git://aufs.git.sourceforge.net/gitroot/aufs/aufs3-standalone.git
 cd aufs3-standalone
 git branch -r
-git checkout origin/aufs3.7
+git checkout origin/aufs3.x-rcN
 
 # modify what you want
 
@@ -15,16 +15,16 @@ git checkout origin/aufs3.7
 rm -v $(find . -type f -name '*.orig')
 grep -qse 'EXPORT_SYMBOL(' aufs3-standalone.patch && \
 sed -i-old -e 's|EXPORT_SYMBOL(|EXPORT_SYMBOL_GPL(|' aufs3-standalone.patch
-rm -rf ../tmp/linux-3.7
-mkdir -p ../tmp/linux-3.7
-cp -a fs ../tmp/linux-3.7
-cp -a include ../tmp/linux-3.7
-cp -a Documentation ../tmp/linux-3.7
-rm ../tmp/linux-3.7/include/uapi/linux/Kbuild
+rm -rf ../tmp/linux-3.x-rcN
+mkdir -p ../tmp/linux-3.x-rcN
+cp -a fs ../tmp/linux-3.x-rcN
+cp -a include ../tmp/linux-3.x-rcN
+cp -a Documentation ../tmp/linux-3.x-rcN
+rm ../tmp/linux-3.x-rcN/include/uapi/linux/Kbuild
 cd ../tmp
-diff -Naur null linux-3.7  | filterdiff | \
-sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-3.7-old\1|;s|^--- null.*|--- /dev/null|;\|linux-3.7/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
-| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-3.7/include/linux/aufs_type.h).patch.bz2
+diff -Naur null linux-3.x-rcN  | filterdiff | \
+sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-3.x-rcN-old\1|;s|^--- null.*|--- /dev/null|;\|linux-3.x-rcN/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
+| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-3.x-rcN/include/linux/aufs_type.h).patch.bz2
 mv *.bz2 $OLDPWD
 cd $OLDPWD
 mv *patch* ..
