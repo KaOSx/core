b8b151cefae352b4b7c90e21a775bb154c9fcc01
demmm <anke62@gmail.com>
2014-12-15 19:03:34 +0000

    prep linux-next for latest aufs patches add 3.18 config file, adjuts FW_loader settings for systemd218 changes linux stable config systemd 218 firmware loader adjustments linux-firmware rebuild to let the kernel find firmware by symlinking

--- linux-next/create-aufs.sh
+++ linux-next/create-aufs.sh
@@ -5,7 +5,7 @@ rm -Rf aufs3-standalone
 git clone git://git.code.sf.net/p/aufs/aufs3-standalone aufs3-standalone
 cd aufs3-standalone
 git branch -r
-git checkout origin/aufs3.17
+git checkout origin/aufs3.18
 
 # modify what you want
 
@@ -15,16 +15,16 @@ git checkout origin/aufs3.17
 rm -v $(find . -type f -name '*.orig')
 grep -qse 'EXPORT_SYMBOL(' aufs3-standalone.patch && \
 sed -i-old -e 's|EXPORT_SYMBOL(|EXPORT_SYMBOL_GPL(|' aufs3-standalone.patch
-rm -rf ../tmp/linux-3.17
-mkdir -p ../tmp/linux-3.17
-cp -a fs ../tmp/linux-3.17
-cp -a include ../tmp/linux-3.17
-cp -a Documentation ../tmp/linux-3.17
-rm ../tmp/linux-3.17/include/uapi/linux/Kbuild
+rm -rf ../tmp/linux-3.18
+mkdir -p ../tmp/linux-3.18
+cp -a fs ../tmp/linux-3.18
+cp -a include ../tmp/linux-3.18
+cp -a Documentation ../tmp/linux-3.18
+rm ../tmp/linux-3.18/include/uapi/linux/Kbuild
 cd ../tmp
-diff -Naur null linux-3.17  | filterdiff | \
-sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-3.17-old\1|;s|^--- null.*|--- /dev/null|;\|linux-3.17/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
-| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-3.17/include/uapi/linux/aufs_type.h).patch.bz2
+diff -Naur null linux-3.18  | filterdiff | \
+sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-3.18-old\1|;s|^--- null.*|--- /dev/null|;\|linux-3.18/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
+| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-3.18/include/uapi/linux/aufs_type.h).patch.bz2
 mv *.bz2 $OLDPWD
 cd $OLDPWD
 mv *patch* ..
