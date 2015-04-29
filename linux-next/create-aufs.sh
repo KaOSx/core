1e32af334f045e5e4ab06487a2a81a09314c338f
demmm <anke62@gmail.com>
2015-04-29 02:11:31 +0000

    mirror-check move kaosx.tk as main mirror linux-next add all new aufs4 patches prep 4.0.1 build, aufs moved to github

--- linux-next/create-aufs.sh
+++ linux-next/create-aufs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-rm -Rf aufs3-standalone
+rm -Rf aufs4-standalone
 # clone the aufs repository to the local disk
-git clone git://git.code.sf.net/p/aufs/aufs3-standalone aufs3-standalone
-cd aufs3-standalone
+git clone git://github.com/sfjro/aufs4-standalone.git aufs4-standalone.git
+cd aufs4-standalone.git
 git branch -r
-git checkout origin/aufs3.19
+git checkout origin/aufs4.0
 
 # modify what you want
 
@@ -13,23 +13,23 @@ git checkout origin/aufs3.19
 # create the patch with the directories: fs, include and Documentation
 
 rm -v $(find . -type f -name '*.orig')
-grep -qse 'EXPORT_SYMBOL(' aufs3-standalone.patch && \
-sed -i-old -e 's|EXPORT_SYMBOL(|EXPORT_SYMBOL_GPL(|' aufs3-standalone.patch
-rm -rf ../tmp/linux-3.19
-mkdir -p ../tmp/linux-3.19
-cp -a fs ../tmp/linux-3.19
-cp -a include ../tmp/linux-3.19
-cp -a Documentation ../tmp/linux-3.19
-rm ../tmp/linux-3.19/include/uapi/linux/Kbuild
+grep -qse 'EXPORT_SYMBOL(' aufs4-standalone.patch && \
+sed -i-old -e 's|EXPORT_SYMBOL(|EXPORT_SYMBOL_GPL(|' aufs4-standalone.patch
+rm -rf ../tmp/linux-4.0
+mkdir -p ../tmp/linux-4.0
+cp -a fs ../tmp/linux-4.09
+cp -a include ../tmp/linux-4.0
+cp -a Documentation ../tmp/linux-4.0
+rm ../tmp/linux-4.0/include/uapi/linux/Kbuild
 cd ../tmp
-diff -Naur null linux-3.19  | filterdiff | \
-sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-3.19-old\1|;s|^--- null.*|--- /dev/null|;\|linux-3.19/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
-| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-3.19/include/uapi/linux/aufs_type.h).patch.bz2
+diff -Naur null linux-4.0  | filterdiff | \
+sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-4.0-old\1|;s|^--- null.*|--- /dev/null|;\|linux-4.0/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
+| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-4.0/include/uapi/linux/aufs_type.h).patch.bz2
 mv *.bz2 $OLDPWD
 cd $OLDPWD
 mv *patch* ..
 cd ..
-rm -rf tmp aufs3-standalone
+rm -rf tmp aufs4-standalone
 
 
 # the patch is created in ../tmp
