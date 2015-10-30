dd5ef4dd7450ac8c691c0734391ef744b3798d75
demmm <anke62@gmail.com>
2015-10-30 02:07:32 +0000

    move linux to 4.2.5

--- linux/create-aufs.sh
+++ linux/create-aufs.sh
@@ -5,7 +5,7 @@ rm -Rf aufs4-standalone
 git clone git://github.com/sfjro/aufs4-standalone.git aufs4-standalone
 cd aufs4-standalone
 git branch -r
-git checkout origin/aufs4.0
+git checkout origin/aufs4.2
 
 # modify what you want
 
@@ -15,16 +15,16 @@ git checkout origin/aufs4.0
 rm -v $(find . -type f -name '*.orig')
 grep -qse 'EXPORT_SYMBOL(' aufs4-standalone.patch && \
 sed -i-old -e 's|EXPORT_SYMBOL(|EXPORT_SYMBOL_GPL(|' aufs4-standalone.patch
-rm -rf ../tmp/linux-4.0
-mkdir -p ../tmp/linux-4.0
-cp -a fs ../tmp/linux-4.0
-cp -a include ../tmp/linux-4.0
-cp -a Documentation ../tmp/linux-4.0
-rm ../tmp/linux-4.0/include/uapi/linux/Kbuild
+rm -rf ../tmp/linux-4.2
+mkdir -p ../tmp/linux-4.2
+cp -a fs ../tmp/linux-4.2
+cp -a include ../tmp/linux-4.2
+cp -a Documentation ../tmp/linux-4.2
+rm ../tmp/linux-4.1/include/uapi/linux/Kbuild
 cd ../tmp
-diff -Naur null linux-4.0  | filterdiff | \
-sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-4.0-old\1|;s|^--- null.*|--- /dev/null|;\|linux-4.0/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
-| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-4.0/include/uapi/linux/aufs_type.h).patch.bz2
+diff -Naur null linux-4.2  | filterdiff | \
+sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-4.2-old\1|;s|^--- null.*|--- /dev/null|;\|linux-4.2/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
+| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-4.2/include/uapi/linux/aufs_type.h).patch.bz2
 mv *.bz2 $OLDPWD
 cd $OLDPWD
 mv *patch* ..
