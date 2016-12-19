b31fc8f6b45cc1227fa36fa01bcd917af1a22719
demmm <anke62@gmail.com>
2016-12-19 02:28:28 +0000

    libpsl 0.16.1, .so back down to 5 wget & curl rebuild for .so down pyhton2 2.7.13 aufs patchs updated linux-next, prep nexy build

--- linux-next/create-aufs.sh
+++ linux-next/create-aufs.sh
@@ -5,7 +5,7 @@ rm -Rf aufs4-standalone
 git clone git://github.com/sfjro/aufs4-standalone.git aufs4-standalone
 cd aufs4-standalone
 git branch -r
-git checkout origin/aufs4.8
+git checkout origin/aufs4.9
 
 # modify what you want
 
@@ -15,16 +15,16 @@ git checkout origin/aufs4.8
 rm -v $(find . -type f -name '*.orig')
 grep -qse 'EXPORT_SYMBOL(' aufs4-standalone.patch && \
 sed -i-old -e 's|EXPORT_SYMBOL(|EXPORT_SYMBOL_GPL(|' aufs4-standalone.patch
-rm -rf ../tmp/linux-4.8
-mkdir -p ../tmp/linux-4.8
-cp -a fs ../tmp/linux-4.8
-cp -a include ../tmp/linux-4.8
-cp -a Documentation ../tmp/linux-4.8
-rm ../tmp/linux-4.8/include/uapi/linux/Kbuild
+rm -rf ../tmp/linux-4.9
+mkdir -p ../tmp/linux-4.9
+cp -a fs ../tmp/linux-4.9
+cp -a include ../tmp/linux-4.9
+cp -a Documentation ../tmp/linux-4.9
+rm ../tmp/linux-4.9/include/uapi/linux/Kbuild
 cd ../tmp
-diff -Naur null linux-4.8  | filterdiff | \
-sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-4.8-old\1|;s|^--- null.*|--- /dev/null|;\|linux-4.8/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
-| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-4.8/include/uapi/linux/aufs_type.h).patch.bz2
+diff -Naur null linux-4.9  | filterdiff | \
+sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-4.9-old\1|;s|^--- null.*|--- /dev/null|;\|linux-4.9/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
+| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-4.9/include/uapi/linux/aufs_type.h).patch.bz2
 mv *.bz2 $OLDPWD
 cd $OLDPWD
 mv *patch* ..
