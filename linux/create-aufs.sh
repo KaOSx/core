f5172517660a0a32546a5707232774952e19a2be
demmm <anke62@gmail.com>
2017-06-15 16:17:31 +0000

    move linux to 4.11.5 aufs patches updated, config set linux-next added 2 configs for next build, 2 more header files added

--- linux/create-aufs.sh
+++ linux/create-aufs.sh
@@ -5,7 +5,7 @@ rm -Rf aufs4-standalone
 git clone git://github.com/sfjro/aufs4-standalone.git aufs4-standalone
 cd aufs4-standalone
 git branch -r
-git checkout origin/aufs4.10
+git checkout origin/aufs4.11.0-untested
 
 # modify what you want
 
@@ -15,16 +15,16 @@ git checkout origin/aufs4.10
 rm -v $(find . -type f -name '*.orig')
 grep -qse 'EXPORT_SYMBOL(' aufs4-standalone.patch && \
 sed -i-old -e 's|EXPORT_SYMBOL(|EXPORT_SYMBOL_GPL(|' aufs4-standalone.patch
-rm -rf ../tmp/linux-4.10
-mkdir -p ../tmp/linux-4.10
-cp -a fs ../tmp/linux-4.10
-cp -a include ../tmp/linux-4.10
-cp -a Documentation ../tmp/linux-4.10
-rm ../tmp/linux-4.10/include/uapi/linux/Kbuild
+rm -rf ../tmp/linux-4.11
+mkdir -p ../tmp/linux-4.11
+cp -a fs ../tmp/linux-4.11
+cp -a include ../tmp/linux-4.11
+cp -a Documentation ../tmp/linux-4.11
+rm ../tmp/linux-4.11/include/uapi/linux/Kbuild
 cd ../tmp
-diff -Naur null linux-4.10  | filterdiff | \
-sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-4.10-old\1|;s|^--- null.*|--- /dev/null|;\|linux-4.10/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
-| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-4.10/include/uapi/linux/aufs_type.h).patch.bz2
+diff -Naur null linux-4.11  | filterdiff | \
+sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-4.11-old\1|;s|^--- null.*|--- /dev/null|;\|linux-4.11/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
+| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-4.11/include/uapi/linux/aufs_type.h).patch.bz2
 mv *.bz2 $OLDPWD
 cd $OLDPWD
 mv *patch* ..
