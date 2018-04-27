40ef3bab4ed0743abe5947cbedf82335409d88de
demmm <anke62@gmail.com>
2018-04-27 10:24:42 +0000

    linux  move to 4.16 updated to 4.16.5, aufs patches updated, objtool installed for nvidia, remove doc section, added elfutils depend

--- linux/create-aufs.sh
+++ linux/create-aufs.sh
@@ -5,7 +5,7 @@ rm -Rf aufs4-standalone
 git clone git://github.com/sfjro/aufs4-standalone.git aufs4-standalone
 cd aufs4-standalone
 git branch -r
-git checkout origin/aufs4.15
+git checkout origin/aufs4.16
 
 # modify what you want
 
@@ -15,16 +15,16 @@ git checkout origin/aufs4.15
 rm -v $(find . -type f -name '*.orig')
 grep -qse 'EXPORT_SYMBOL(' aufs4-standalone.patch && \
 sed -i-old -e 's|EXPORT_SYMBOL(|EXPORT_SYMBOL_GPL(|' aufs4-standalone.patch
-rm -rf ../tmp/linux-4.15
-mkdir -p ../tmp/linux-4.15
-cp -a fs ../tmp/linux-4.15
-cp -a include ../tmp/linux-4.15
-cp -a Documentation ../tmp/linux-4.15
-rm ../tmp/linux-4.15/include/uapi/linux/Kbuild
+rm -rf ../tmp/linux-4.16
+mkdir -p ../tmp/linux-4.16
+cp -a fs ../tmp/linux-4.16
+cp -a include ../tmp/linux-4.16
+cp -a Documentation ../tmp/linux-4.16
+rm ../tmp/linux-4.16/include/uapi/linux/Kbuild
 cd ../tmp
-diff -Naur null linux-4.15  | filterdiff | \
-sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-4.15-old\1|;s|^--- null.*|--- /dev/null|;\|linux-4.15/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
-| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-4.15/include/uapi/linux/aufs_type.h).patch.bz2
+diff -Naur null linux-4.16  | filterdiff | \
+sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-4.16-old\1|;s|^--- null.*|--- /dev/null|;\|linux-4.16/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
+| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-4.16/include/uapi/linux/aufs_type.h).patch.bz2
 mv *.bz2 $OLDPWD
 cd $OLDPWD
 mv *patch* ..
