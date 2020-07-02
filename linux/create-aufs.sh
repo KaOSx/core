47b78ec81d69d9eec8b37f60797e779833985ad3
demmm <anke62@gmail.com>
2020-07-02 11:10:33 +0000

    linux 5.7.7 updated aufs patches, config bridge-utils 1.7, new src url

--- linux/create-aufs.sh
+++ linux/create-aufs.sh
@@ -5,7 +5,7 @@ rm -Rf aufs5-standalone
 git clone git://github.com/sfjro/aufs5-standalone.git aufs5-standalone
 cd aufs5-standalone
 git branch -r
-git checkout origin/aufs5.6
+git checkout origin/aufs5.7
 
 # modify what you want
 
@@ -15,16 +15,16 @@ git checkout origin/aufs5.6
 rm -v $(find . -type f -name '*.orig')
 grep -qse 'EXPORT_SYMBOL(' aufs5-standalone.patch && \
 sed -i-old -e 's|EXPORT_SYMBOL(|EXPORT_SYMBOL_GPL(|' aufs5-standalone.patch
-rm -rf ../tmp/linux-5.6
-mkdir -p ../tmp/linux-5.6
-cp -a fs ../tmp/linux-5.6
-cp -a include ../tmp/linux-5.6
-cp -a Documentation ../tmp/linux-5.6
-rm ../tmp/linux-5.6/include/uapi/linux/Kbuild
+rm -rf ../tmp/linux-5.7
+mkdir -p ../tmp/linux-5.7
+cp -a fs ../tmp/linux-5.7
+cp -a include ../tmp/linux-5.7
+cp -a Documentation ../tmp/linux-5.7
+rm ../tmp/linux-5.7/include/uapi/linux/Kbuild
 cd ../tmp
-diff -Naur null linux-5.6  | filterdiff | \
-sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-5.6-old\1|;s|^--- null.*|--- /dev/null|;\|linux-5.6/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
-| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-5.6/include/uapi/linux/aufs_type.h).patch.bz2
+diff -Naur null linux-5.7  | filterdiff | \
+sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-5.7-old\1|;s|^--- null.*|--- /dev/null|;\|linux-5.7/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
+| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-5.7/include/uapi/linux/aufs_type.h).patch.bz2
 mv *.bz2 $OLDPWD
 cd $OLDPWD
 mv *patch* ..
