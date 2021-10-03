c5eaaf51bddc9a308c78b5d2104ce5c07bdbdded
demmm <anke62@gmail.com>
2021-10-03 19:57:22 +0000

    linux 5.14.9 updated aufs & config, added header file

--- linux/create-aufs.sh
+++ linux/create-aufs.sh
@@ -5,7 +5,7 @@ rm -Rf aufs5-standalone
 git clone git://github.com/sfjro/aufs5-standalone.git aufs5-standalone
 cd aufs5-standalone
 git branch -r
-git checkout origin/aufs5.13
+git checkout origin/aufs5.14
 
 # modify what you want
 
@@ -15,16 +15,16 @@ git checkout origin/aufs5.13
 rm -v $(find . -type f -name '*.orig')
 grep -qse 'EXPORT_SYMBOL(' aufs5-standalone.patch && \
 sed -i-old -e 's|EXPORT_SYMBOL(|EXPORT_SYMBOL_GPL(|' aufs5-standalone.patch
-rm -rf ../tmp/linux-5.13
-mkdir -p ../tmp/linux-5.13
-cp -a fs ../tmp/linux-5.13
-cp -a include ../tmp/linux-5.13
-cp -a Documentation ../tmp/linux-5.13
-rm ../tmp/linux-5.13/include/uapi/linux/Kbuild
+rm -rf ../tmp/linux-5.14
+mkdir -p ../tmp/linux-5.14
+cp -a fs ../tmp/linux-5.14
+cp -a include ../tmp/linux-5.14
+cp -a Documentation ../tmp/linux-5.14
+rm ../tmp/linux-5.14/include/uapi/linux/Kbuild
 cd ../tmp
-diff -Naur null linux-5.13  | filterdiff | \
-sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-5.13-old\1|;s|^--- null.*|--- /dev/null|;\|linux-5.13/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
-| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-5.13/include/uapi/linux/aufs_type.h).patch.bz2
+diff -Naur null linux-5.14  | filterdiff | \
+sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-5.14-old\1|;s|^--- null.*|--- /dev/null|;\|linux-5.14/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
+| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-5.14/include/uapi/linux/aufs_type.h).patch.bz2
 mv *.bz2 $OLDPWD
 cd $OLDPWD
 mv *patch* ..
