de6b976806783dc7bd9769f4439779bd99446c80
demmm <anke62@gmail.com>
2022-09-18 19:39:42 +0000

    prep linux move to 5.19.10, including dracut option updated aufs patches, added dracut-install file, has a fallback img line, needs testing added zfs-kmod, now that linux also has the dracut option
    kbd & kmod updated home url, prep next build

--- linux/create-aufs.sh
+++ linux/create-aufs.sh
@@ -5,7 +5,7 @@ rm -Rf aufs5-standalone
 git clone https://github.com/sfjro/aufs5-standalone.git aufs5-standalone
 cd aufs5-standalone
 git branch -r
-git checkout origin/aufs5.18
+git checkout origin/aufs5.19
 
 # modify what you want
 
@@ -15,16 +15,16 @@ git checkout origin/aufs5.18
 rm -v $(find . -type f -name '*.orig')
 grep -qse 'EXPORT_SYMBOL(' aufs5-standalone.patch && \
 sed -i-old -e 's|EXPORT_SYMBOL(|EXPORT_SYMBOL_GPL(|' aufs5-standalone.patch
-rm -rf ../tmp/linux-5.18
-mkdir -p ../tmp/linux-5.18
-cp -a fs ../tmp/linux-5.18
-cp -a include ../tmp/linux-5.18
-cp -a Documentation ../tmp/linux-5.18
-rm ../tmp/linux-5.18/include/uapi/linux/Kbuild
+rm -rf ../tmp/linux-5.19
+mkdir -p ../tmp/linux-5.19
+cp -a fs ../tmp/linux-5.19
+cp -a include ../tmp/linux-5.19
+cp -a Documentation ../tmp/linux-5.19
+rm ../tmp/linux-5.19/include/uapi/linux/Kbuild
 cd ../tmp
-diff -Naur null linux-5.18  | filterdiff | \
-sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-5.18-old\1|;s|^--- null.*|--- /dev/null|;\|linux-5.18/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
-| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-5.18/include/uapi/linux/aufs_type.h).patch.bz2
+diff -Naur null linux-5.19  | filterdiff | \
+sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-5.19-old\1|;s|^--- null.*|--- /dev/null|;\|linux-5.19/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
+| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-5.19/include/uapi/linux/aufs_type.h).patch.bz2
 mv *.bz2 $OLDPWD
 cd $OLDPWD
 mv *patch* ..
