a83b25a52461f7c58a427f8e31029872c9f66c2e
demmm <anke62@gmail.com>
2024-03-27 13:27:25 +0000

    curl rebuild to remove conflicting fish file use completion from fish for now updated aufs patches, prep next build

--- linux-next/create-aufs.sh
+++ linux-next/create-aufs.sh
@@ -5,7 +5,7 @@ rm -Rf aufs-standalone
 git clone https://github.com/sfjro/aufs-standalone.git aufs6-standalone
 cd aufs6-standalone
 git branch -r
-git checkout origin/aufs6.7
+git checkout origin/aufs6.8
 
 # modify what you want
 
@@ -15,16 +15,16 @@ git checkout origin/aufs6.7
 rm -v $(find . -type f -name '*.orig')
 grep -qse 'EXPORT_SYMBOL(' aufs6-standalone.patch && \
 sed -i-old -e 's|EXPORT_SYMBOL(|EXPORT_SYMBOL_GPL(|' aufs6-standalone.patch
-rm -rf ../tmp/linux-6.7
-mkdir -p ../tmp/linux-6.7
-cp -a fs ../tmp/linux-6.7
-cp -a include ../tmp/linux-6.7
-cp -a Documentation ../tmp/linux-6.7
-rm ../tmp/linux-6.7/include/uapi/linux/Kbuild
+rm -rf ../tmp/linux-6.8
+mkdir -p ../tmp/linux-6.8
+cp -a fs ../tmp/linux-6.8
+cp -a include ../tmp/linux-6.8
+cp -a Documentation ../tmp/linux-6.8
+rm ../tmp/linux-6.8/include/uapi/linux/Kbuild
 cd ../tmp
-diff -Naur null linux-6.7  | filterdiff | \
-sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-6.7-old\1|;s|^--- null.*|--- /dev/null|;\|linux-6.7/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
-| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-6.7/include/uapi/linux/aufs_type.h).patch.bz2
+diff -Naur null linux-6.8  | filterdiff | \
+sed -e 's|null\(/include/uapi/linux/Kbuild\)|linux-6.8-old\1|;s|^--- null.*|--- /dev/null|;\|linux-6.8/include/uapi/linux/Kbuild|,${\|@@ -0,0 +1 @@|,$d}' \
+| bzip2 > aufs$(sed -ne 's|#define.*AUFS_VERSION.*"\(.*\)"|\1|p'  linux-6.8/include/uapi/linux/aufs_type.h).patch.bz2
 mv *.bz2 $OLDPWD
 cd $OLDPWD
 mv *patch* ..
